01b2a2e82e09fd48fb1eef30b8c6718fc48233be
MurphyPOne <MurphyP1@bishopireton.org>
2019-04-10 18:28:28 -0400

    Updates

---
+++ precache-manifest.1574faab04113168cd4898b3a1d1bde4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c28536a7701c438f6c47",
+    "url": "/MurphyPone/react-bracket-generator/static/js/runtime~main.b4d9c979.js"
+  },
+  {
+    "revision": "83f636e4c1cd86d606bb",
+    "url": "/MurphyPone/react-bracket-generator/static/js/main.96f668eb.chunk.js"
+  },
+  {
+    "revision": "74fb1d839f386f7b98ea",
+    "url": "/MurphyPone/react-bracket-generator/static/js/2.571dc9c8.chunk.js"
+  },
+  {
+    "revision": "f43215dab040e5bd74dd64519c6b4d6f",
+    "url": "/MurphyPone/react-bracket-generator/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/MurphyPone/react-bracket-generator/precache-manifest.1574faab04113168cd4898b3a1d1bde4.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/MurphyPone/react-bracket-generator/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
